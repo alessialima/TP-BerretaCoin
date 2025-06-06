822bc2e064909b51725edbbcaf44ef9a82c55563
Margitic <165930893+Margitic@users.noreply.github.com>
2025-06-06 10:58:01 -0300

    Update HeapTransacciones.java

--- código/HeapTransacciones.java
+++ código/HeapTransacciones.java
@@ -1,5 +1,13 @@
 package aed;
 
 public class HeapTransacciones {
+// comparamos primero el monto y luego
+// desempatamos por mayor id
+    private int compararTransacciones(Transaccion tx1, Transaccion tx2) {
+    if (tx1.monto() != tx2.monto()) {
+        return tx2.monto() - tx1.monto();
+    }
+    return tx2.id() - tx1.id(); 
+}
     
 }
