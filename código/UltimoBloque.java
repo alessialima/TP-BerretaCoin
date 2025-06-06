9f1ed3390bddcc558e2a56a2635f4a6f6d098127
Margitic <165930893+Margitic@users.noreply.github.com>
2025-06-06 01:51:05 -0300

    Update UltimoBloque.java

--- código/UltimoBloque.java
+++ código/UltimoBloque.java
@@ -4,5 +4,24 @@ package aed;
 //la suma total de montos y cantidad de transacciones
 //(excluyendo de creacion)
 public class UltimoBloque {
+    private Transaccion[] tx;
+    private boolean[] txSinHackear;
+    private int montoTotalTx;
+    private int cantTotalTx;
+
+    public UltimoBloque(Transaccion[] tx){
+        this.tx = tx;
+        this.txSinHackear = new boolean[tx.length];
+        this.montoTotalTx = 0;
+        this.cantTotalTx = 0;
+
+        for (int i = 0; i < tx.length; i++){
+            txSinHackear[i] = true; 
+            if (tx[i].id_comprador() != 0){
+                montoTotalTx += tx[i].monto();
+                cantTotalTx ++;
+            }
+        }
+    }
     
 }
