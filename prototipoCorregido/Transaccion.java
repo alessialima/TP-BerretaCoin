9c53bfe85384f883c438d65bb4f805e4428fe10b
alessia <lima.alessialourdes@gmail.com>
2025-06-10 11:44:21 -0300

    Update Transaccion.java

--- prototipoCorregido/Transaccion.java
+++ prototipoCorregido/Transaccion.java
@@ -1,12 +1,12 @@
 package aed;
-//agregamos handle para poder encontrar las transacciones en O(1)
-public class Transaccion implements Comparable<Transaccion> , HeapHandle {
+
+public class Transaccion implements Comparable<Transaccion> , HeapHandle { //agregamos handle para poder encontrar las transacciones en O(1)
     private int id;
     private int id_comprador;
     private int id_vendedor;
     private int monto;
     private int indiceHeap; 
-    private ListaEnlazada.Nodo<Transaccion> nodoLista; // referencio al nodo 
+    private ListaEnlazada.Nodo<Transaccion> nodoLista; //referencio al nodo 
 
     public Transaccion(int id, int id_comprador, int id_vendedor, int monto) {
         this.id = id;
@@ -14,14 +14,13 @@ public class Transaccion implements Comparable<Transaccion> , HeapHandle {
         this.id_vendedor = id_vendedor;
         this.monto = monto;
         this.indiceHeap = -1;
-        // cuando se inserte la transaccion dentro del heap,
-        //el handle sera modificado por su correspondiente valor
+        //cuando se inserte la transaccion dentro del heap, el handle sera modificado por su correspondiente valor
     }
 
     @Override
     public int compareTo(Transaccion otro) {
         if (this.monto != otro.monto) {
-            return this.monto - otro.monto; // devuelve la transaccion de mayor monto
+            return this.monto - otro.monto; //devuelve la transaccion de mayor monto
         }
         return this.id - otro.id; //desempata con mayor id
     }
@@ -30,11 +29,11 @@ public class Transaccion implements Comparable<Transaccion> , HeapHandle {
     public boolean equals(Object o) {
         if (this == o) {
             return true;
-        }    // si ambas transacciones apuntan al mismo lugar en memoria seran iguales
+        }    //si ambas transacciones apuntan al mismo lugar, en terminos de memoria seran iguales
         else {
             if (o == null || getClass() != o.getClass()) {
              return false;
-            } //si es nula o de distinta clase seran distintas
+            } //si es nula o son de distinta clase, seran distintas
             else {
             Transaccion that = (Transaccion) o;
             return id == that.id;
@@ -42,7 +41,7 @@ public class Transaccion implements Comparable<Transaccion> , HeapHandle {
             //asumimos que las transacciones tienen id unico dentro de toda la blockchain,
             //no solo dentro del bloque
             //de todas maneras es indiferente debido a que solo trabajaremos con el ultimo
-            // bloque de la blockchain
+            //bloque de la blockchain
         }
         }
     }
